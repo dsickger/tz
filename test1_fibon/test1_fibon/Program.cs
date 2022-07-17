07676a17cc2eaf907ef4302224b83bb110cb5bc9
dsickger <91094958+dsickger@users.noreply.github.com>
2022-07-17 15:55:42 +0300

    тз1 фибоначчи

---
+++ test1_fibon/test1_fibon/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("введите чколько чисел вывести");
+int n =Convert.ToInt32(Console.ReadLine());
+int f1 = 0, f2 = 1,f_sum;
+Console.WriteLine("последовательность Фибоначчи:");
+while(n>0)
+{
+    Console.WriteLine(f1);
+    n--;
+    f_sum = f1 + f2;
+    f1 = f2;
+    f2 = f_sum;
+}
+
